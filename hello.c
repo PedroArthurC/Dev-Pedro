3c67d572b74a95c64a4993c1aed988ec2bd16c70
LucasHeilbuth <lucasheilbuth@yahoo.com.br>
2022-09-17 13:05:56 -0300

    Revert "Adicionado funcionalidade de soma"
    This reverts commit 106df0b731e1052ea9430f0f1c40ca7fa07c79b0.

--- hello.c
+++ hello.c
@@ -1,9 +1,7 @@
-  #include<stdio.h>
+ #include<stdio.h>
   
   int main(){
-    int ret;
-    ret = soma(2, 3);
-    printf("O resultado é: %d", ret);
+    printf("Hello World!");
     return 0;
   }
   #Comentario 1
